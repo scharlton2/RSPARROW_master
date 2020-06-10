c5577a59405f94ecd09c54ee1da2455da86b5e2f
Gorman Sanisaca <lgormansanisaca@usgs.gov>
2020-06-10 14:27:53 -0400

    change name of installPackages

--- R/installRSPARROWpackages.R
+++ R/installRSPARROWpackages.R
@@ -1,4 +1,4 @@
-#'@title installPackages
+#'@title installRSPARROWpackages
 #'@description installs all required packages for RSPARROW in batch mode \\cr \\cr
 #'Executed By: sparrow_control.R \\cr
 #'Executes Routines: pkgInstall.R \\cr
@@ -8,7 +8,7 @@
 #'       'RSPARROW_master/R/' subdirectory
 
 
-installPackages <- function(if_install_packages,path_master){
+installRSPARROWpackages <- function(if_install_packages,path_master){
   
   if(substr(path_master,nchar(path_master),nchar(path_master))==.Platform$file.sep){
     path_master<-substr(path_master,1,nchar(path_master)-1)
@@ -55,7 +55,9 @@ installPackages <- function(if_install_packages,path_master){
                 "htmltools",
                 "htmlwidgets",
                 "plotly",
-                "leaflet.extras"
+                "leaflet.extras",
+                "mapview",
+                "magrittr"
     )
     
     #get path to Rscript.exe
